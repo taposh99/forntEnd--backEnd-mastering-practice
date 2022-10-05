0e0154621afab861fde9f9b865729a0a0f69dbea
Saniatul Haque Taposh <taposhmd2@gmail.com>
2022-10-05 16:58:41 +0600

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        return view('frontEnd.home.home');
+
+    } public function productDetails(){
+        return view('frontEnd.product.productDetails');
+
+    }
+}
